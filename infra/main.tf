6b4f51c9db1f933e1b796b2d46cd815ffa6f5f5c
Diego Iglesias Bayo <38340341+diegoib@users.noreply.github.com>
2026-06-16 07:50:50 +0200

    chore: cleaning (#14)

--- infra/main.tf
+++ infra/main.tf
@@ -76,22 +76,22 @@ module "compute" {
 }
 
 module "cloud_run" {
-  source     = "./modules/cloud-run"
-  project_id = var.project_id
-  region     = var.region
-  serving_sa = module.iam.serving_sa_email
-  redis_host = module.compute.streaming_vm_internal_ip
-  model_dir  = "gs://${module.gcs.bucket_name}/models"
+  source           = "./modules/cloud-run"
+  project_id       = var.project_id
+  region           = var.region
+  serving_sa       = module.iam.serving_sa_email
+  redis_host       = module.compute.streaming_vm_internal_ip
+  model_dir        = "gs://${module.gcs.bucket_name}/models"
   mlflow_uri       = "http://${module.compute.streaming_vm_static_ip}:5000"
   redpanda_brokers = "${module.compute.streaming_vm_internal_ip}:9092"
 }
 
 module "cloud_run_jobs" {
-  source        = "./modules/cloud-run-jobs"
-  project_id    = var.project_id
-  region        = var.region
-  jobs_sa       = module.iam.jobs_sa_email
-  mlflow_uri    = "http://${module.compute.streaming_vm_static_ip}:5000"
+  source          = "./modules/cloud-run-jobs"
+  project_id      = var.project_id
+  region          = var.region
+  jobs_sa         = module.iam.jobs_sa_email
+  mlflow_uri      = "http://${module.compute.streaming_vm_static_ip}:5000"
   gcs_data_path   = "gs://${module.gcs.bucket_name}/processed/train_dataset.parquet"
   gcs_movies_path = "gs://${module.gcs.bucket_name}/raw/movies.csv"
   gcs_onnx_out    = "gs://${module.gcs.bucket_name}/models/model.onnx"
@@ -104,11 +104,11 @@ module "secrets" {
   project_id = var.project_id
 }
 
-output "datagen_vm_ip"         { value = module.compute.datagen_vm_external_ip }
-output "bucket_name"           { value = module.gcs.bucket_name }
-output "bucket_url"            { value = module.gcs.bucket_url }
-output "repository_url"        { value = module.artifact_registry.repository_url }
-output "streaming_vm_ip"       { value = module.compute.streaming_vm_external_ip }
+output "datagen_vm_ip" { value = module.compute.datagen_vm_external_ip }
+output "bucket_name" { value = module.gcs.bucket_name }
+output "bucket_url" { value = module.gcs.bucket_url }
+output "repository_url" { value = module.artifact_registry.repository_url }
+output "streaming_vm_ip" { value = module.compute.streaming_vm_external_ip }
 output "streaming_vm_static_ip" { value = module.compute.streaming_vm_static_ip }
 output "streaming_vm_internal" { value = module.compute.streaming_vm_internal_ip }
-output "serving_url"           { value = module.cloud_run.service_url }
+output "serving_url" { value = module.cloud_run.service_url }
